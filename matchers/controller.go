24406809d3abcac1625a306ee68ece78dc2d3c28
ovechkin-dm <dovechkin@gmail.com>
2025-04-16 16:03:31 +0200

    add handler abstraction

--- matchers/controller.go
+++ matchers/controller.go
@@ -1,22 +1,25 @@
 package matchers
 
-import "github.com/ovechkin-dm/mockio/v2/config"
+import (
+	"reflect"
+	"github.com/ovechkin-dm/mockio/v2/config"
+)
+
+type MockEnv struct {
+	Reporter ErrorReporter
+	Config   *config.MockConfig	
+}
 
 type MockController struct {
-	Reporter ErrorReporter
-	Config   *config.MockConfig
+	Env *MockEnv
+	MockFactory MockFactory
 }
 
-func NewMockController(reporter ErrorReporter, opts ...config.Option) *MockController {
-	cfg := config.NewConfig()
-	if reporter == nil {
-		panic("MockController: provided a nil error reporting (*testing.T) instance")
-	}
-	for _, opt := range opts {
-		opt(cfg)
-	}
-	return &MockController{
-		Reporter: reporter,
-		Config:   cfg,
-	}
+type MockFactory interface {
+	 BuildHandler(env *MockEnv, ifaceType reflect.Type) Handler
 }
+
+type Handler interface {
+	Handle(method reflect.Method, values []reflect.Value) []reflect.Value	
+}
+
