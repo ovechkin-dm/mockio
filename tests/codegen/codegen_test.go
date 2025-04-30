f4c5022ecf10a916cd70a065b4198ab9cd763f28
ovechkin-dm <ovechkin.dm@gmail.com>
2025-04-30 16:39:22 +0200

    fix tests

--- tests/codegen/codegen_test.go
+++ tests/codegen/codegen_test.go
@@ -18,9 +18,10 @@ func TestGeneratedMock(t *testing.T) {
 	result := m.Greet("John")
 	r.AssertEqual("Hello, John", result)
 	Verify(m, Once()).Greet("John")
+	r.AssertNoError()
 }
 
-func TestGeneratedMockNoMoreInteractions(t *testing.T) {
+func TestGeneratedMockNoMoreInteractionsSuccess(t *testing.T) {
 	r := common.NewMockReporter(t)
 	ctrl := NewMockController(r)
 	m := NewMockGreeter(ctrl)
@@ -29,5 +30,20 @@ func TestGeneratedMockNoMoreInteractions(t *testing.T) {
 	})
 	result := m.Greet("John")
 	r.AssertEqual("Hello, John", result)
+	Verify(m, Once()).Greet("John")
 	VerifyNoMoreInteractions(m)
+	r.AssertNoError()
+}
+
+func TestGeneratedMockNoMoreInteractionsFail(t *testing.T) {
+	r := common.NewMockReporter(t)
+	ctrl := NewMockController(r)
+	m := NewMockGreeter(ctrl)
+	WhenSingle(m.Greet(AnyString())).ThenAnswer(func(args []any) string {
+		return "Hello, " + args[0].(string)
+	})
+	result := m.Greet("John")
+	r.AssertEqual("Hello, John", result)	
+	VerifyNoMoreInteractions(m)
+	r.AssertError()
 }
